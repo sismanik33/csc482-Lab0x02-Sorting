9e37f40b74d98f3252483f641ddef6ab66783709
Sean Cicmanec <sean.cicmanec@gmail.com>
2020-09-17 10:59:45 -0500

    first commit

---
+++ src/lab0x02.java
@@ -0,0 +1,14 @@
+public class lab0x02 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] quicksort(int [] array){
+        if (array.length < 2){
+            return array;
+        }
+        int len = array.length;
+//        int pivotIndex =
+        return array;
+    }
+}
